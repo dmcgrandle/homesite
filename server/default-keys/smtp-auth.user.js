da9df3130e9bd48fb1b4fa44f33b42093013550b
Darren McGrandle <darren@mcgrandle.com>
2019-03-02 06:17:29 -0800

    setup defaults for keys and startcontainer script

---
+++ server/default-keys/smtp-auth.user.js
@@ -0,0 +1,5 @@
+const auth = {
+    user : 'webmaster@example.com',
+    pass : 'SecretPass'
+}
+module.exports = auth;
--- startcontainer.sh
+++ startcontainer.sh
@@ -3,4 +3,27 @@
 # Startup script to copy configs and start processes
 #
 
+# Create the keys directory (doesn't exist in docker image)
+mkdir /homesite/keys
+
+# Copy in appropriate keys
+if [ -d /homesite/config/keys ]
+then
+    cp /homesite/config/keys/* /homesite/keys
+else
+    cp /homesite/default-keys/* /homesite/keys
+fi
+
+# Set up /protected directories if they don't exist
+if [ ! -d /homesite/protected ]
+then
+    mkdir /homesite/protected
+    mkdir /homesite/protected/images
+    mkdir /homesite/protected/videos
+    mkdir /homesite/protected/downloads
+    cp /homesite/public/assets/images/Mountain.jpg /homesite/protected/images
+    cp /homesite/public/assets/images/Mountain.jpg /homesite/protected/downloads
+    cp /homesite/public/assets/video/Beach* /homesite/protected/videos
+fi
+
 supervisord -n -c /etc/supervisor/conf.d/supervisord.conf
