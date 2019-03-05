097a68e0a3bd6a201568481f4ca1968004b1a10e
Darren McGrandle <darren@mcgrandle.com>
2019-03-05 13:36:00 -0800

    fix startcontainer to include copying in custom configs

--- startcontainer.sh
+++ startcontainer.sh
@@ -3,7 +3,7 @@
 # Startup script to copy configs and start processes
 #
 
-# redirect logs to the stdout of PID 1
+# Anything appended to the stdout of PID 1 will appear in "docker logs" for the container
 LOGLOC=/proc/1/fd/1
 
 # Create the keys directory (doesn't exist in docker image)
@@ -13,6 +13,7 @@ mkdir /homesite/keys
 if [ -d /homesite/config/keys ]
 then
     cp -f /homesite/config/keys/* /homesite/keys
+    echo "Found custom keys and copied them into the container." >> $LOGLOC
 fi
 
 # Set up /protected directories if they don't exist
@@ -39,19 +40,26 @@ fi
 if [ -d /homesite/config/public/assets/config ]
 then
     cp /homesite/config/public/assets/config/* /homesite/public/assets/config
-    echo "Copied custom client configs" >> $LOGLOC
+    echo "Custom client configs found and copied into the container." >> $LOGLOC
 fi
 
 # copy in custom public image and video files if they exist
 if [ -d /homesite/config/public/assets/images ]
 then
     cp /homesite/config/public/assets/images/* /homesite/public/assets/images
-    echo "Copied custom public images into container" >> $LOGLOC
+    echo "Custom public images found and copied into the container." >> $LOGLOC
 fi
 if [ -d /homesite/config/public/assets/video ]
 then
     cp /homesite/config/public/assets/video/* /homesite/public/assets/video
-    echo "Copied custom public videos into container" >> $LOGLOC
+    echo "Custom public videos found and copied into the container." >> $LOGLOC
+fi
+
+# Copy over custom server config file if it exists
+if [ -f /homesite/config/server.config.js ]
+then
+    cp /homesite/config/server.config.js /homesite//assets/config
+    echo "Custom client configs found and copied into the container." >> $LOGLOC
 fi
 
 
