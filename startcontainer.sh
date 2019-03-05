42553aff7330eb2e745d2beefca08861110765f9
Darren McGrandle <darren@mcgrandle.com>
2019-03-05 13:23:36 -0800

    fix logging from startup script

--- startcontainer.sh
+++ startcontainer.sh
@@ -3,6 +3,9 @@
 # Startup script to copy configs and start processes
 #
 
+# redirect logs to the stdout of PID 1
+LOGLOC=/proc/1/fd/1
+
 # Create the keys directory (doesn't exist in docker image)
 mkdir /homesite/keys
 
@@ -15,19 +18,19 @@ fi
 # Set up /protected directories if they don't exist
 if [ ! -d /homesite/protected/images ]
 then
-    echo "No directory /protected/images - creating with defaults."
+    echo "No directory /protected/images - creating with defaults." >> $LOGLOC
     mkdir /homesite/protected/images
     cp /homesite/public/assets/images/Mountain.jpg /homesite/protected/images
 fi
 if [ ! -d /homesite/protected/videos ]
 then
-    echo "No directory /protected/videos - creating with defaults."
+    echo "No directory /protected/videos - creating with defaults." >> $LOGLOC
     mkdir /homesite/protected/videos
     cp /homesite/public/assets/video/Beach* /homesite/protected/videos
 fi
 if [ ! -d /homesite/protected/downloads ]
 then
-    echo "No directory /protected/downloads - creating with defaults."
+    echo "No directory /protected/downloads - creating with defaults." >> $LOGLOC
     mkdir /homesite/protected/downloads
     cp /homesite/public/assets/images/Mountain.jpg /homesite/protected/downloads
 fi
@@ -36,16 +39,19 @@ fi
 if [ -d /homesite/config/public/assets/config ]
 then
     cp /homesite/config/public/assets/config/* /homesite/public/assets/config
+    echo "Copied custom client configs" >> $LOGLOC
 fi
 
 # copy in custom public image and video files if they exist
 if [ -d /homesite/config/public/assets/images ]
 then
     cp /homesite/config/public/assets/images/* /homesite/public/assets/images
+    echo "Copied custom public images into container" >> $LOGLOC
 fi
 if [ -d /homesite/config/public/assets/video ]
 then
     cp /homesite/config/public/assets/video/* /homesite/public/assets/video
+    echo "Copied custom public videos into container" >> $LOGLOC
 fi
 
 
