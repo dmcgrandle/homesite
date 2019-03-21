7f7f9261669028f10dc104eaa8f4efdf83686be8
Darren McGrandle <darren@mcgrandle.com>
2019-03-20 23:38:27 -0700

    update card component - increase readability.

--- client/src/app/shared/card/card.component.ts
+++ client/src/app/shared/card/card.component.ts
@@ -1,28 +1,22 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'shared-card',
     template: `
-    <mat-card>
-        <mat-card-header>
-            <mat-card-title>
-                <h3 class="card-title">{{name}}</h3>
-            </mat-card-title>
-            <mat-card-subtitle>{{description}}</mat-card-subtitle>
-        </mat-card-header>
-        <img mat-card-image [src]='image | secure' (load)="loaded.emit(true)">
-    </mat-card>`,
-    styles: [`
-    .mat-card {
-        background-color: #eaf6fd;
-    }
-    .card-title {
-        font-size: 0.8em;
-        margin: 0;
-    }
-    .mat-card-image {
-        border-radius: 3px;
-    }`]
+        <mat-card>
+            <mat-card-header>
+                <mat-card-title>
+                    <h3>{{name}}</h3>
+                </mat-card-title>
+                <mat-card-subtitle>{{description}}</mat-card-subtitle>
+            </mat-card-header>
+            <img mat-card-image [src]='image | secure' (load)="loaded.emit(true)">
+        </mat-card>`,
+    styles: [
+        '.mat-card { background-color: #eaf6fd; }',
+        'h3 { font-size: 0.8em; margin: 0; }',
+        'img { border-radius: 3px; }'
+    ]
 })
 export class CardComponent {
 
